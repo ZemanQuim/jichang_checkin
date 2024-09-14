5ad6ed7c2affcbea70e281c822f318e00fe1fd74
ZemanQuim <zq849330701@gmail.com>
2024-09-14 17:30:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
                 exit()
         user_quantity = len(configs)
         user_quantity = user_quantity // 2
-         print(f'配置文件{configs}')
+        print(f'账号：{configs}')
         for i in range(user_quantity):
                 user = configs[i*2]
                 pwd = configs[i*2+1]
